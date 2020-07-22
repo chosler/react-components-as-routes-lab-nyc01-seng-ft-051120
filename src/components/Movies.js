2528fb41ef7b1351b11a98a0e0f9e8647df4fece
CMSTie <cmstie@gmail.com>
2020-07-22 15:10:05 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return <div>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <ul>Genres:
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          </div>}
+          )}
     </div>
   );
 };
